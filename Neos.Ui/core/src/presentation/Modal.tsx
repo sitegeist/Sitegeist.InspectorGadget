ad0b02bfd3649542b3386c3fd67477b0ce031ebd
Wilhelm Behncke <wilhelm.behncke@protonmail.com>
2023-07-31 17:53:55 +0200

    BUGFIX: Use native Neos UI Dialog for inspector editor

--- Neos.Ui/core/src/presentation/Modal.tsx
+++ Neos.Ui/core/src/presentation/Modal.tsx
@@ -1,47 +1,19 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import cx from 'classnames';
+
+import {Dialog} from '@neos-project/react-ui-components';
 
 export const Modal: React.FC<{
     renderTitle(): React.ReactNode
     renderBody(): React.ReactNode
 }> = props => ReactDOM.createPortal(
-    <div
-        data-ignore_click_outside="true"
-        className={cx(
-            'sg-ig-fixed',
-            'sg-ig-inset-0',
-            'sg-ig-bg-[rgba(0,0,0,.9)]',
-            'sg-ig-animate-overlay-appear',
-            'sg-ig-z-[4]'
-        )}
+    <Dialog
+        isOpen={true}
+        title={props.renderTitle()}
+        onRequestClose={() => {}}
+        preventClosing
     >
-        <div
-            className={cx(
-                'sg-ig-absolute',
-                'sg-ig-top-1/2',
-                'sg-ig-left-1/2',
-                'sg-ig-transform',
-                'sg-ig--translate-x-1/2 sg-ig--translate-y-1/2 sg-ig-scale-100',
-                'sg-ig-bg-gray-900',
-                'sg-ig-shadow-[0,20px,40px,rgba(0,0,0,.4)]',
-                'sg-ig-opacity-100',
-                'sg-ig-animate-modal-appear',
-                'sg-ig-border-2 sg-ig-border-gray-800'
-            )}
-        >
-            <div
-                className={cx(
-                    'sg-ig-text-large',
-                    'sg-ig-leading-tight',
-                    'sg-ig-p-4 sg-ig-pr-[40px]'
-                )}
-            >
-                {props.renderTitle()}
-            </div>
-
-            {props.renderBody()}
-        </div>
-    </div>,
+        {props.renderBody()}
+    </Dialog>,
     document.body
 );
