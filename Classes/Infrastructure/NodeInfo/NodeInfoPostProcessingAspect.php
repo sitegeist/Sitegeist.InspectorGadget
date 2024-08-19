c3956fe33c4b4b2cb448eef8246aac78194191de
Bernhard Schmitt <schmitt@sitegeist.de>
2024-08-19 11:45:08 +0200

    Adjust to Neos9 beta 11

--- Classes/Infrastructure/NodeInfo/NodeInfoPostProcessingAspect.php
+++ Classes/Infrastructure/NodeInfo/NodeInfoPostProcessingAspect.php
@@ -2,6 +2,7 @@
 namespace Sitegeist\InspectorGadget\Infrastructure\NodeInfo;
 
 use Neos\ContentRepository\Core\Projection\ContentGraph\Node;
+use Neos\ContentRepositoryRegistry\ContentRepositoryRegistry;
 use Neos\Flow\Annotations as Flow;
 use Neos\Flow\Aop\JoinPointInterface;
 
@@ -10,6 +11,11 @@ use Neos\Flow\Aop\JoinPointInterface;
  */
 final class NodeInfoPostProcessingAspect
 {
+    public function __construct(
+        private readonly ContentRepositoryRegistry $contentRepositoryRegistry,
+    ) {
+    }
+
     /**
      * @Flow\Around("method(Neos\Neos\Ui\Fusion\Helper\NodeInfoHelper->renderNodeWithPropertiesAndChildrenInformation())")
      * @param JoinPointInterface $joinPoint
@@ -20,12 +26,14 @@ final class NodeInfoPostProcessingAspect
     ):?array {
         /** @var Node $node */
         $node = $joinPoint->getMethodArgument('node');
-        $nodeType = $node->nodeType;
+        $nodeType = $this->contentRepositoryRegistry->get($node->contentRepositoryId)
+            ->getNodeTypeManager()
+            ->getNodeType($node->nodeTypeName);
         /** @var array<mixed>|null $result */
         $result = $joinPoint->getAdviceChain()->proceed($joinPoint);
 
         foreach ($result['properties'] ?? [] as $propertyName => &$convertedValue) {
-            $editorName = $nodeType->getConfiguration(
+            $editorName = $nodeType?->getConfiguration(
                 sprintf('properties.%s.ui.inspector.editor', $propertyName)
             );
 
